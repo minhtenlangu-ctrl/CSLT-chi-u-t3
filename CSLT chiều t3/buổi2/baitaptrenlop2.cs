9d101f900793928bdc9a343270d87350c78855e7
Tangdobaongoc <minhtenlangu@gmail.com>
2026-08-26 15:06:49 +0700

    bt1

---
+++ CSLT chiều t3/buổi2/baitaptrenlop2.cs
@@ -0,0 +1,22 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSLT_chiều_t3.buổi2
+{
+    internal class baitaptrenlop2
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.Write("Nhập bán kính hình cầu: ");
+            double radius = Convert.ToDouble(Console.ReadLine());
+
+            double surface = 4 * Math.PI * Math.Pow(radius, 2);
+            double volume = (4.0 / 3.0) * Math.PI * Math.Pow(radius, 3);
+
+            Console.WriteLine($"Diện tích mặt cầu (Surface): {surface:F2}");
+            Console.WriteLine($"Thể tích khối cầu (Volume): {volume:F1}");
+        }
+    }
+}*/
---
+++ CSLT chiều t3/buổi2/baitaptrenlop3.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSLT_chiều_t3.buổi2
+{
+    internal class baitaptrenlop3
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Console.Write("Nhập số thứ nhất: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Nhập số thứ hai: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\n--- Kết quả phép tính ---");
+            Console.WriteLine($"Phép cộng: {a} + {b} = {a + b}");
+            Console.WriteLine($"Phép trừ:  {a} - {b} = {a - b}");
+            Console.WriteLine($"Phép nhân: {a} x {b} = {a * b}");
+            Console.WriteLine($"Phép chia: {a} / {b} = {a / b}");
+            Console.WriteLine($"Chia lấy dư (mod): {a} mod {b} = {a % b}");
+        }
+    }
+}
